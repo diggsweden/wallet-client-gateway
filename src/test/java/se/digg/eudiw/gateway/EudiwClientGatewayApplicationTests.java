a1bf72b68060c6563a83235a6d46e058db75d84a
jahwag <540380+jahwag@users.noreply.github.com>
2025-08-21 19:17:29 +0200

    fix: unnecessary import removed
    Signed-off-by: jahwag <540380+jahwag@users.noreply.github.com>

--- src/test/java/se/digg/eudiw/gateway/EudiwClientGatewayApplicationTests.java
+++ src/test/java/se/digg/eudiw/gateway/EudiwClientGatewayApplicationTests.java
@@ -6,7 +6,6 @@ package se.digg.eudiw.gateway;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
-import org.springframework.test.context.ActiveProfiles;
 
 @Import(TestcontainersConfiguration.class)
 @SpringBootTest
