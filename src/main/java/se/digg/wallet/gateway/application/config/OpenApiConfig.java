6ca582c763db343e52560b02fca932ae3a8a6f93
Markus Beer <markus.beer@digg.se>
2025-09-29 15:38:46 +0200

    feat: add swagger auth button and use relative url
    Signed-off-by: Markus Beer <markus.beer@digg.se>

---
+++ src/main/java/se/digg/wallet/gateway/application/config/OpenApiConfig.java
@@ -0,0 +1,35 @@
+// SPDX-FileCopyrightText: 2025 Digg - Agency for Digital Government
+//
+// SPDX-License-Identifier: EUPL-1.2
+
+package se.digg.wallet.gateway.application.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import jakarta.servlet.ServletContext;
+import java.util.List;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+  @Bean
+  public OpenAPI openApi(ServletContext servletContext) {
+    return new OpenAPI()
+        .info(new Info().title("Wallet Client Gateway").version("v1"))
+        .servers(List.of(new Server().url(servletContext.getContextPath())))
+        .components(new Components()
+            .addSecuritySchemes("ApiKeyScheme",
+                new SecurityScheme()
+                    .type(SecurityScheme.Type.APIKEY)
+                    .in(SecurityScheme.In.HEADER)
+                    .name(ApiKeyAuthFilter.API_KEY_HEADER)))
+        .addSecurityItem(new SecurityRequirement().addList("ApiKeyScheme"));
+  }
+
+}
