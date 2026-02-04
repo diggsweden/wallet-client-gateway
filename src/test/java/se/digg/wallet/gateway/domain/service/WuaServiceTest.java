2beb8ee0c247298453c21ff850e3f7928f0c3510
Michael Danielsson <extern.michael.danielsson@digg.se>
2026-02-04 13:58:58 +0100

    fix: add WuaService test
    Signed-off-by: Michael Danielsson <extern.michael.danielsson@digg.se>

--- src/test/java/se/digg/wallet/gateway/domain/service/WuaServiceTest.java
+++ src/test/java/se/digg/wallet/gateway/domain/service/WuaServiceTest.java
@@ -10,6 +10,7 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
 import static org.mockito.Mockito.when;
 
+import java.util.Optional;
 import java.util.UUID;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -20,7 +21,11 @@ import se.digg.wallet.gateway.application.model.CreateWuaDtoTestBuilder;
 import se.digg.wallet.gateway.application.model.wua.WuaDto;
 import se.digg.wallet.gateway.domain.service.wua.WuaMapper;
 import se.digg.wallet.gateway.domain.service.wua.WuaService;
+import se.digg.wallet.gateway.infrastructure.account.client.WalletAccountClient;
+import se.digg.wallet.gateway.infrastructure.account.model.WalletAccountAccountDto;
+import se.digg.wallet.gateway.infrastructure.account.model.WalletAccountJwkDto;
 import se.digg.wallet.gateway.infrastructure.walletprovider.client.WalletProviderClient;
+import se.digg.wallet.gateway.infrastructure.walletprovider.model.WalletProviderCreateWuaDto;
 import se.digg.wallet.gateway.infrastructure.walletprovider.model.WalletProviderCreateWuaDtoV1;
 
 @ExtendWith(MockitoExtension.class)
@@ -34,10 +39,14 @@ class WuaServiceTest {
   @Mock
   private WuaMapper wuaMapper;
 
+  @Mock
+  private WalletAccountClient accountClient;
+
   @InjectMocks
   private WuaService wuaService;
 
 
+  @Deprecated(forRemoval = true)
   @Test
   void createAttributeSuccess() {
     // Given
@@ -55,6 +64,38 @@ class WuaServiceTest {
     // Then
     assertEquals(expectedWua, actualWuaDto);
     verify(client).createWua(any(WalletProviderCreateWuaDtoV1.class));
+    verifyNoMoreInteractions(client);
+  }
+
+  @Test
+  void createWua() {
+    // Given
+    var accountId = UUID.randomUUID();
+    var nonce = "nonce";
+    var publicKey = new WalletAccountJwkDto("kty", "kid", "alg", "use", "crv", "x", "y");
+    var accountDto = new WalletAccountAccountDto(accountId, "", "",
+        Optional.empty(), publicKey);
+    var expectedWua = new WuaDto("my dummy jwt");
+    var mappedDto = new WalletProviderCreateWuaDto("data", "nonce");
+
+    when(accountClient.getAccount(accountId.toString()))
+        .thenReturn(Optional.of(accountDto));
+    when(wuaMapper.toWalletProviderCreateWuaDto(accountDto, nonce))
+        .thenReturn(mappedDto);
+    when(client.createWua(mappedDto))
+        .thenReturn(expectedWua.jwt());
+
+    // When
+    var actualWuaDto = wuaService.createWua(accountId.toString(), nonce);
+
+    // Then
+    assertEquals(expectedWua, actualWuaDto);
+    verify(accountClient).getAccount(any(String.class));
+    verifyNoMoreInteractions(accountClient);
+    verify(wuaMapper).toWalletProviderCreateWuaDto(any(WalletAccountAccountDto.class),
+        any(String.class));
+    verifyNoMoreInteractions(wuaMapper);
+    verify(client).createWua(any(WalletProviderCreateWuaDto.class));
     verifyNoMoreInteractions(client);
   }
 }
