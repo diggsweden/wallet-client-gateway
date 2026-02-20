0363736be738e7278f258d1d7e11b2659fe9390f
Asser Hakala <extern.asser.hakala@digg.se>
2026-02-20 13:39:34 +0100

    feat: remove wua/v3 endpoint
    * feat: remove wua/v3
    
    Signed-off-by: Johan Forsman <extern.johan.forsman@digg.se>
    
    * fix(wua): correct params in OpenAPI spec
    
    Signed-off-by: Asser Hakala <extern.asser.hakala@digg.se>
    
    * fix: remove curly brackets
    
    Signed-off-by: Johan Forsman <extern.johan.forsman@digg.se>
    
    ---------
    
    Signed-off-by: Johan Forsman <extern.johan.forsman@digg.se>
    Signed-off-by: Asser Hakala <extern.asser.hakala@digg.se>
    Co-authored-by: Johan Forsman <extern.johan.forsman@digg.se>

--- src/main/java/se/digg/wallet/gateway/application/controller/openapi/wua/CreateWuaOpenApiDocumentation.java
+++ src/main/java/se/digg/wallet/gateway/application/controller/openapi/wua/CreateWuaOpenApiDocumentation.java
@@ -5,9 +5,7 @@
 package se.digg.wallet.gateway.application.controller.openapi.wua;
 
 import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.ExampleObject;
 import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
@@ -29,17 +27,5 @@ import se.digg.wallet.gateway.application.model.wua.WuaDto;
 @ApiResponse(description = "Bad input", responseCode = "400",
     content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
         schema = @Schema(implementation = BadRequestDto.class)))
-@RequestBody(content = @Content(
-    examples = @ExampleObject("""
-            {
-              "walletId": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
-              "jwk": {
-                "kty": "EC",
-                "crv": "P-256",
-                "x": "1fH0eqXgMMwCIafNaDc1axdCjLlw7zpTLvLWjpPvhEc",
-                "y": "5qOejJs7BK-jLingaUTEhBrzP_YPyHfptS5yWE98I40"
-              }
-            }
-        """)))
 public @interface CreateWuaOpenApiDocumentation {
 }
