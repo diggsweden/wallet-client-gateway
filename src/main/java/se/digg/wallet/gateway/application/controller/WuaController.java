773844dc4557667593ca1cf66859d2fff335740f
Shirlene Lim <extern.shirlene.lim@digg.se>
2026-03-18 11:49:40 +0100

    fix: validate wua authentication
    Signed-off-by: Shirlene Lim <extern.shirlene.lim@digg.se>

---
+++ src/main/java/se/digg/wallet/gateway/application/controller/WuaController.java
@@ -0,0 +1,56 @@
+// SPDX-FileCopyrightText: 2025 Digg - Agency for Digital Government
+//
+// SPDX-License-Identifier: EUPL-1.2
+
+package se.digg.wallet.gateway.application.controller;
+
+import java.util.Optional;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import se.digg.wallet.gateway.application.auth.ChallengeResponseAuthentication;
+import se.digg.wallet.gateway.application.controller.openapi.wua.CreateWuaOpenApiDocumentation;
+import se.digg.wallet.gateway.application.model.wua.WuaDto;
+import se.digg.wallet.gateway.domain.service.wua.WuaService;
+
+@RestController
+@RequestMapping("/wua")
+@Validated
+public class WuaController {
+  private final Logger logger = LoggerFactory.getLogger(WuaController.class);
+  private final WuaService wuaService;
+
+  public WuaController(WuaService wuaService) {
+    this.wuaService = wuaService;
+  }
+
+  @PostMapping()
+  @CreateWuaOpenApiDocumentation
+  public ResponseEntity<WuaDto> createWua(
+      ChallengeResponseAuthentication challengeResponseAuthentication,
+      @RequestParam Optional<String> nonce) {
+    if (challengeResponseAuthentication == null) {
+      logger.warn("Received request with empty challenge response authentication");
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+    }
+    if (challengeResponseAuthentication.getAccountId() == null) {
+      logger.warn("Received request with empty account id");
+      return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+    }
+    if (nonce.isPresent() && nonce.get().isBlank()) {
+      logger.warn("Received request with empty nonce");
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+    }
+    logger.debug("Received request from account id: {}, nonce: {}",
+        challengeResponseAuthentication.getAccountId(), nonce.orElse(""));
+    WuaDto wuaDto = wuaService.createWua(challengeResponseAuthentication.getAccountId(),
+        nonce.orElse(""));
+    return ResponseEntity.status(HttpStatus.CREATED).body(wuaDto);
+  }
+}
